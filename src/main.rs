b39d1a630cb6369b66990faffc37d013d492724a
comblock <81411315+comblock@users.noreply.github.com>
2022-09-10 20:30:45 +0200

    rocket hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+// We deny unsafe code because we want to be as secure as possible.
+// If we really need it we can allow it on a case by case basis.
+#[deny(clippy::pedantic, unsafe_code)]
+#[forbid(clippy::undocumented_unsafe_blocks)]
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
